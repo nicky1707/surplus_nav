bf8c2f4ef8ed6faae8baec68582397b4f0061abf
Priya Sundar S <85740988+nicky1707@users.noreply.github.com>
2024-05-13 16:52:04 +0530

    host

--- surplus_nav/settings.py
+++ surplus_nav/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,8 +26,8 @@ SECRET_KEY = 'django-insecure-h%v6-a^)rz3!ejzv#$j*i+7ahnxw9m(v471i)((0vk#!+ikygo
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["surplus-nav.onrender.com"]
-
+ALLOWED_HOSTS = ["*"]
+PORT = int(os.environ.get("PORT", 8000))
 
 # Application definition
 
